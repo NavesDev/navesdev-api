331d9dd1484ed9fa0f0b62d21ab7317bafec09f4
NavesDev <davinaves.2006@gmail.com>
2025-06-01 21:31:48 -0300

    Level 1 API
    This API can count my projects access.

--- api/index.ts
+++ api/index.ts
@@ -1,38 +1,133 @@
-import {fastify} from "fastify";
+import { fastify } from "fastify";
 import cors from "@fastify/cors";
-import rl from "@fastify/rate-limit";
+import ratelimit from "@fastify/rate-limit";
+import cookies from "@fastify/cookie";
+import mysql from "mysql2";
+require("dotenv").config();
 
-const app = fastify();
+const app = fastify({
+  trustProxy: true,
+});
 
-app.register(cors,{
-    origin : "*" //"http://navesdev.github.io"
-})
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: parseInt(process.env.DB_PORT , 10),
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 5,
+  queueLimit: 0,
+});
 
-app.register(rl,{
-    max : 60,
-    timeWindow: '1h'
-})
+app.register(cors, {
+  origin: "*", //"http://navesdev.github.io"
+});
 
-const apiRegs = (app)=> {
-    app.register(rl,{
-        max : 8,
-        timeWindow:"1m"
-    })
-    
-    app.get("/websites/newacess/:name", (request, reply)=>{
-        const name = request.params.name
+app.register(ratelimit, {
+  max: 3,
+  timeWindow: 1000*60*2,
+  global: true
+});
 
-        return reply.status(404).send({ error: "Usuário não encontrado" });
-    })
+app.register(cookies);
+
+async function searchDataByName(name: string, wantedData: string) {
+  const query = `SELECT ${wantedData} FROM website WHERE wname = ?`; //const query = "SELECT waccess FROM website WHERE wname = ?"
+  try {
+    const [rows]: any[] = await pool.promise().query(query, [name]);
+    const data = rows.length > 0 ? rows[0] : null;
+    if (data) {
+      return {
+        status: true,
+        response: data,
+      };
+    } else {
+      return {
+        status: false,
+        message: "Website não encontrado.",
+        code: 404,
+      };
+    }
+  } catch (error) {
+    return {
+      status: false,
+      message: "Erro interno, tente novamente mais tarde.",
+      code: 500
+    };
+  }
 }
 
-app.register(apiRegs)
+app.get("/", () => {
+  return `Rotas disponíveis : '/websites', '/websites/NOMEDOSITE', '/websites/NOMEDOSITE/newaccess'`;
+});
 
-app.get("/",()=>{
-    return "Hello Word"
-})
+app.get("/websites", async (request, reply) => {
+  try {
+    const [rows]: any[] = await pool
+      .promise()
+      .query("SELECT wname,waccess,wcdate FROM website");
+    if (rows.length > 0) {
+      return reply.code(200).send({
+        status: true,
+        response: rows,
+      });
+    } else {
+      return reply.code(404).send({
+        status: false,
+        message: "Não foram encontrados websites por algum motivo.",
+      });
+    }
+  } catch (error) {
+    return reply.code(500).send({
+      status: false,
+      message:
+        "Houve um erro de comunicação no servidor, tente novamente mais tarde.",
+    });
+  }
+});
 
+app.get("/websites/:name", async (request: any, reply) => {
+  const name = request.params.name;
+  const response = await searchDataByName(name, "waccess,wname,wcdate");
 
-app.listen({port : 1607}).then(()=>{
-    console.log("API Online!")
-})
+  return response;
+});
+
+app.get("/websites/:name/newaccess", async (request: any, reply) => {
+  const name = request.params.name;
+  const canAcess = request.cookies[`<acess-${name}>`];
+  try {
+    if (!canAcess) {
+      const [result]: any = await pool
+        .promise()
+        .query("UPDATE website SET waccess = waccess + 1 WHERE wname= ?", [
+          name,
+        ]);
+      if (result.affectedRows === 0) {
+        return reply
+          .status(404)
+          .send({ status: false, message: "Website não encontrado" });
+      } else {
+        return reply
+          .setCookie(`<acess-${name}>`, "true", {
+            httpOnly: true,
+            maxAge: 60 * 3,
+            secure: true,
+          })
+          .send({ status: true });
+      }
+    } else{
+		return reply.code(429).send({status:false,message:"Acesso recente percebido"})
+	}
+  } catch (error) {
+    return reply.code(500).send({
+      status: false,
+      message: "Erro de comunicação interno, tente mais tarde",
+    });
+  }
+});
+
+app.listen({ port: 1607 }).then(() => {
+  console.log("API Online!");
+});
