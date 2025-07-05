d25841785f851deeb16c423a0c2ee7ef86b4f38a
NavesDev <davinaves.2006@gmail.com>
2025-07-05 16:20:19 -0300

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -479,8 +479,9 @@ async function bootstrap() {
        const baseUrl = process.env.VERCEL_URL 
         ? `https://${process.env.VERCEL_URL}` 
         : `http://localhost:${process.env.PORT || 1607}`;
-
+        console.log(baseUrl)
         const pythonApiUrl = `${baseUrl}/internal/aiservice/camisai`;
+        console.log(pythonApiUrl)
         let response = await fetch(
           pythonApiUrl, //"http:127.0.0.1:5001/camisai"
           {
@@ -537,10 +538,6 @@ async function bootstrap() {
       }
     }
   );
-
-  await app.get("/internal/aiservice/camisai",()=>{
-    return "teste 123"
-  });
 
   return app;
 }
